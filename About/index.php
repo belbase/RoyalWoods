9bc9afde055e08e09f33573929c3b04171b549a3
Deepak Belbase <mads.belbase@gmail.com>
2016-08-18 13:05:43 +0530

    Create index.php

---
+++ About/index.php
@@ -0,0 +1,43 @@
+<?php 
+include_once('../resources/php_lib/elements.php');
+include_once('../resources/php_lib/functions.php');
+require('../rw-config.php');
+
+    rw_meta('About -RoyalWoods');
+    rw_header();
+?>
+<div id='rw_main_doc'>
+    <div class='rw_frame_one'><center><h1>About</h1></center>
+        
+        <div class='rw_content_frame'>
+            <img id="rw_featured" src="<?php echo ROOT_HT;?>/resources/images/123456.78912_p.jpg"/>
+        </div>
+        <div class='rw_content_frame' style="height:500px">
+            <div class='rw_frame_two'>
+                <h2>OH HI THERE </h2>
+                <h3>WELCOME TO ROYALWOODS</h3>
+                 <p>We are the future of modern furniture.
+                    We serve the best furniture product 
+                    specialize in living room product and home Décors.
+                </p>
+                
+                <h3>WHERE IT ALL BEGINS</h3>
+               <p>Orginate from local market from Ludhiana royal wood was once known as
+                    DS furniture in his old days and rebuild as royal wood in 2009 since
+                    then its an Indian furniture brand that serve best quality wooden furniture <br/>
+                    Royalwood was founded by Alok K. Verma and Mukesh Khanna the local woodcutter.
+                    Alok K. Verma  identify the skill of Mukesh Khanna and start a furniture shop in late 
+                    90s. The start to work as local carpenter  who built on the order of customer in 2002
+                    setup there tile business and letter they expand it with  Manufacturing  couch , sofa and chairs.
+                </p>
+                <h3>HIGH FASHION  FORWARD AND AFFORDABLE</h3>
+                 <p>Royalwoods understand the current trend and design the which meet the expectation of the consumer .
+                </p>
+            </div>
+        </div></div>
+
+
+
+<!-- Footer File Coding Script-->
+
+<?php   rw_footer();
