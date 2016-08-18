b09e219b31d10c4028705cab073deec156ca5974
Deepak Belbase <mads.belbase@gmail.com>
2016-08-18 13:04:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
  ********************************************************************
  *   
  */
-include_once('/resources/php_lib/elements.php');
-include_once('/resources/php_lib/functions.php');
+include_once('resources/php_lib/elements.php');
+include_once('resources/php_lib/functions.php');
 require('rw-config.php');
 
     rw_meta('RoyalWoods - The online Furniture Store');
@@ -32,21 +32,21 @@ require('rw-config.php');
 <!--CONTENT DIV END--></div>
     </div><div class='rw_content_frame'>
 <!--1MARQUEE DIV START--><div class="rw_home_content_box">
-    <img src="/resources/images/.85301924176_n.jpg" height="50%" width="100%" />
+    <img src="<?php echo ROOT_HT;?>/resources/images/.85301924176_n.jpg" height="50%" width="100%" />
 							<h3>Bed Room</h3>
 							<p>
                                                             Explore Our Our Wide Range of Collection for Luxury Living
 							</p>
 <!--1MARQUEE DIV END--></div>
 <!--2MARQUEE DIV START--><div class="rw_home_content_box">
-    <img src="/resources/images/.79283461510_n.jpg" height="50%" width="100%" />
+    <img src="<?php echo ROOT_HT;?>/resources/images/.79283461510_n.jpg" height="50%" width="100%" />
 							<h3>Decors</h3>
 							<p>
                                                             Feel The Real Power of Decorating with our Rich Decorating Accessories.
 							</p>
 <!--2MARQUEE DIV END--></div>
 <!--3MARQUEE DIV START--><div class="rw_home_content_box">
-    <img src="/resources/images/964721.10835_n.jpg" />
+    <img src="<?php echo ROOT_HT;?>/resources/images/964721.10835_n.jpg" />
                                                         <h3>Dining</h3>
 							<p>
                                                             We serve best kind of Wooden Furniture For Dinning, especially Dining Tables .
