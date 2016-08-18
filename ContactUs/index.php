289e3b699838e3fd2f6fe384eef51a1725f2bfd4
Deepak Belbase <mads.belbase@gmail.com>
2016-08-18 13:07:01 +0530

    Create index.php

---
+++ ContactUs/index.php
@@ -0,0 +1,51 @@
+<?php 
+include_once('../resources/php_lib/elements.php');
+include_once('../resources/php_lib/functions.php');
+require('../rw-config.php');
+
+    rw_meta('Contact Us-RoyalWoods');
+    rw_header();
+?>
+<div id='rw_main_doc'>
+    <div class='rw_frame_one'><center><h1>Contact Us</h1></center>
+        
+        <div class='rw_content_frame'>
+            <img id="rw_featured" src="<?php echo ROOT_HT;?>/resources/images/122.789089789_o.jpg"/>
+        </div>
+        <br/>
+        <div class="col well"><div class="row"><div class="col-xs-11">
+        <div class="media">
+        <p style="padding: 40px; float: left; font-size:19px; ">"You may contact us through any resource and from anywhere in India to lodge a complaint 
+            and for any suggestion use our feedback form.
+            You can also subscribe through mail or can follow us in twitter and facebook as well."</p></div></div></div></div>
+        <br/>
+        <div class='rw_content_frame'>
+            
+            <div class="rw_image_view" style="text-align: left;">
+                
+                <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true" style="font-size:72px; float:left;"></span><p>
+                    <b>RoyalWoods Wood Workers Limited.
+                    Shop No. 7 Karimpura- Books Market Road,
+                    Near Railway Station, Ludhiana, Punjab-141008</b>
+                </p>
+                <a href="mailto:edeepakbelbase@gmail.com" class="rw_button_green"> Mail us</a>
+                
+                </div>
+            <div class="rw_image_view" style="width:49%;" >
+                <div class="rw_image_view" style="width:90% ;height:90%; border-radius: 10px;" >
+                <h2 style="width:91%; margin:0; padding:20px; background-color:#e38d13; color:#FFFFFF;border-radius:15px 15px 0 0;"> 
+				<span class="glyphicon glyphicon-phone-alt" aria-hidden="true"> Toll Free Number
+				</h2>
+				<p style="width:91%;margin:0; padding:20px;font-size:32px;color:#e38d13;background-color:#F5F1EC;border-radius:0 0 15px 15px;"> <b>1800 140 1700</b></br>
+				
+				</p>
+                </div>
+                
+            </div>
+</div></div>
+
+
+
+<!-- Footer File Coding Script-->
+
+<?php   rw_footer();
